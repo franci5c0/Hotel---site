92c54178bbb323d00c60a77670d7bb345b4c593d
franci5c0 <cursosenaitecnico@gmail.com>
2024-10-29 14:13:30 -0300

    começando outra API

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express'); //importa o framework express
+
+const dotenv = require('dotenv'); //importa o pacote dotenv para gerenciar variáveis de ambiente
+
+const cors = require('cors'); //importa o pacote cors para permitir requisições de diferentes origens
+
+dotenv.config(); //carrega as cariáveis definidas no arquivo .env
